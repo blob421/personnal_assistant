8cea9f097ca6292bae79a9f6cd86a05633d14b8b
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2026-04-09 09:12:10 -0400

    Rotated secrets and purged commit history.

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ ROOT = os.path.dirname(__file__)
 
 DB_PATH = os.path.join(ROOT, 'user_data.sqlite')
 
-OAUTH_GOOGLE_SECRETS_PATH = os.path.join(ROOT, 'secrets', 'client_secrets.json')
+OAUTH_GOOGLE_SECRETS_PATH = os.path.join(ROOT, 'secrets', 'google_secrets.json')
 
 CONFIRMED_PROVIDERS = ['Google']
